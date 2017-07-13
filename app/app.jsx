0396129832a05b54563ec30a12a4f51a1867c5b7
Kuba <quba560@gmail.com>
2017-07-13 19:19:44 +0200

    clean-up

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
+
+
+//var Main = require('Main');
 
 // load foundation
 $(document).foundation();
@@ -11,7 +13,7 @@ require('!style!css!sass!applicationStyles');
 
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path="/" component={Main}>
+    <Route path="/" component={}>
     </Route>
   </Router>,
   document.getElementById('app')
